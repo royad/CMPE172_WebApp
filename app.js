8dc0b6bae07e0f72fd0749e8168d3d6e1af22dc5
royad <roya.delan@gmail.com>
2016-12-08 11:08:00 -0800

    new changes

--- app.js
+++ app.js
@@ -5,14 +5,54 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+//var bootstrap = require('bootstrap');
 
-
-// new
+// db connections
 var mongo = require('mongodb');
 var monk = require('monk');
-var db = monk('localhost:27017/nodetest1');
+var db = monk('mongodb://admin:aminamin64@ds127998.mlab.com:27998/heroku_jqv9x42l');
+
+
+//************************* new db **********************
+// db connections
+//lets require/import the mongodb native drivers.
+var mongodb = require('mongodb');
+
+//We need to work with "MongoClient" interface in order to connect to a mongodb server.
+var MongoClient = mongodb.MongoClient;
+
+// Connection URL. This is where your mongodb server is running.
+
+//(Focus on This Variable)
+var url = 'mongodb://admin:aminamin64@ds127998.mlab.com:27998/heroku_jqv9x42l';      
+//(Focus on This Variable)
+
+// Use connect method to connect to the Server
+  MongoClient.connect(url, function (err, db) {
+  if (err) {
+    console.log('Unable to connect to the mongoDB server. Error:', err);
+  } else {
+    console.log('Connection established to', url);
+
+    // do some work here with the database.
+
+    //Close connection
+    db.close();
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
 
 var app = express();
 
@@ -21,40 +61,53 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Make database accessible to the router
+// Make our db accessible to our router
 app.use(function(req,res,next){
     req.db = db;
     next();
 });
 
-app.use('/', index);
-//new
-
+app.use('/', routes);
 app.use('/users', users);
 
-// catch 404 and forward to error handler
+/// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
-// error handler
+/// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
+
+
+
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,117 @@
 var express = require('express');
 var router = express.Router();
-
-/* GET home page. */
+/* home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-/* GET Hello World page. */
+/* GET New User page. */
 router.get('/project', function(req, res) {
-    res.render('project', { title: 'CMPE 172 Project' });
+    res.render('project', { title: 'Add New User' });
 });
 
-/* GET Userlist page. */
-router.get('/users', function(req, res) {
+
+/*
+router.post('/register', function(req, res) {
+	
+	    var db = req.db;
+	    var email = req.body.email;
+    var pass = req.body.pass;
+	
+	var students = db.get('students');
+	
+	  var query = "SELECT email, pass " +
+             "FROM students WHERE email = ?" +
+			 "AND pass = ?";
+			 
+			if (query == null){
+				res.send("There was a problem adding the information to the database.");
+			}
+			
+					        else {
+            // And forward to success page
+            res.redirect("/");
+        }
+	
+	/*students.findOne({email: email, pass: pass}, function(err, doc) {
+		if(err){
+			res.send("There was a problem adding the information to the database.");
+		}
+		        else {
+            // And forward to success page
+            res.redirect("/");
+        }
+		
+	})
+});*/
+
+/* new admin - sign up */
+router.post('/loginadmin', function(req, res) {
+
+    // Set our internal DB variable
     var db = req.db;
-    var collection = db.get('usercollection');
-    collection.find({},{},function(e,docs){
-        res.render('userlist', {
-            "userlist" : docs
-        });
+
+    // Get our form values. These rely on the "name" attributes
+    var email = req.body.email;
+    var pass = req.body.pass;
+
+    // Set our collection
+    var students = db.get('students');
+
+    // Submit to the DB
+    students.insert({
+        "email" : email,
+        "pass" : pass
+    }, function (err, doc) {
+        if (err) {
+            // If it failed, return error
+            res.send("There was a problem adding the information to the database.");
+        }
+        else {
+            // And forward to success page
+            res.redirect("/");
+        }
     });
 });
+
+/*
+ * login.
+ */
+router.post('/register', function(req, res) {
+    var db = req.db;
+	
+	    var email = req.body.email;
+    var pass = req.body.pass;
+	
+    var admin = db.get('students');
+	
+	/*admin.findOne({email: email, pass: pass}, function(err, doc) {
+		if(err){
+			res.send("There was a problem adding the information to the database.");
+		}
+		        else {
+            // And forward to success page
+            res.redirect("/");
+        }
+		
+	});*/
+    admin.find({email: email, pass: pass},function(e,docs){
+       var result = res.json(docs);
+	   
+	   	/*for (var i =0; i < result.length; i++){
+		var email = result.email;
+		console.log(email);*/
+		
+		if (result.get("email") != null){
+			res.redirect("/");
+		}
+		else{
+			res.send("There was a problem adding the information to the database.");
+		}
+    });
+	
+
+});
+
 
 module.exports = router;
